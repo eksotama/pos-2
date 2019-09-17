c0fba61304f1e6e1f1cdb3663629c69667bf5541
Hegar Garcia <hegargarcia@gmail.com>
2019-09-17 13:19:15 -0500

    Adding Id to mapping function

--- pos/pos/MainPage.xaml.cs
+++ pos/pos/MainPage.xaml.cs
@@ -16,21 +16,21 @@ namespace pos
 
         private async void Go_To_Clients_List_Page(object sender, EventArgs e)
         {
-            Func<List<ListItem>> getClients = () => Client.Get_All().ConvertAll(client => new ListItem { Photo_Path = client.Profile_Url, Title = client.Name, Detail = client.Phone });
+            Func<List<ListItem>> getClients = () => Client.Get_All().ConvertAll(client => new ListItem { Photo_Path = client.Profile_Url, Title = client.Name, Detail = client.Phone, Id = client.Id });
             var page = new ListPage(getClients, typeof(ClientForm), typeof(ClientDetail), "Clientes");
             await Navigation.PushAsync(page);
         }
 
         private async void Go_To_Products_List_Page(object sender, EventArgs e)
         {
-            Func<List<ListItem>> getProducts = () => Product.Get_All().ConvertAll(product => new ListItem { Photo_Path = product.Photo_Url, Title = product.Name, Detail = product.Price });
+            Func<List<ListItem>> getProducts = () => Product.Get_All().ConvertAll(product => new ListItem { Photo_Path = product.Photo_Url, Title = product.Name, Detail = product.Price, Id = product.Id });
             var page = new ListPage(getProducts, typeof(ProductForm), typeof(ProductDetail), "Productos");
             await Navigation.PushAsync(page);
         }
 
         private async void Go_To_Sells_Page(object sender, EventArgs e)
         {
-            //await Navigation.PushAsync(new ListPage(new List<ListItem>()));
+            await Navigation.PushAsync(new Reports());
         }
 
         private async void Go_To_Reports_Page(object sender, EventArgs e)
