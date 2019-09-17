af43f5a43557ba5a0524ce0025724ad89821d851
Hegar Garcia <hegargarcia@gmail.com>
2019-09-17 13:40:59 -0500

    Finishing client detail page

--- pos/pos/ClientDetail.xaml.cs
+++ pos/pos/ClientDetail.xaml.cs
@@ -12,9 +12,49 @@ namespace pos
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ClientDetail : ContentPage
     {
-        public ClientDetail()
+        public Client Client_Info { get; set; }
+
+        public ClientDetail(int id)
         {
             InitializeComponent();
+            Client_Info = Client.Get(id);
+        }
+
+        protected override void OnAppearing()
+        {
+            Name_Entry.Text = Client_Info.Name;
+            Email_Entry.Text = Client_Info.Email;
+            Address_Entry.Text = Client_Info.Address;
+            Phone_Entry.Text = Client_Info.Phone;
+            Photo_Entry.Text = Client_Info.Profile_Url;
+        }
+
+        private void Update_Client(object sender, EventArgs e)
+        {
+            Client_Info.Name = Name_Entry.Text;
+            Client_Info.Email = Email_Entry.Text;
+            Client_Info.Phone = Phone_Entry.Text;
+            Client_Info.Profile_Url = Photo_Entry.Text;
+            Client_Info.Address = Address_Entry.Text;
+
+            Client_Info.Update();
+            Return_To_List();
+        }
+
+        private async void Delete_Client(object sender, EventArgs e)
+        {
+            var acceptDeleting = await DisplayAlert("Eliminar cliente", "Estas seguro de querer borrar al cliente", "OK", "Cancelar");
+
+            if (acceptDeleting)
+            {
+                Client_Info.Delete();
+                Return_To_List();
+            }
+        }
+
+        private async void Return_To_List()
+        {
+            await Application.Current.MainPage.Navigation.PopAsync();
         }
     }
 }
